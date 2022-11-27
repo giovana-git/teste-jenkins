7e256ca7ccd9fda19f2fc8b87e3c18fcbeb96b65
giovana <giovanadealmeida1105@gmail.com>
2022-11-27 20:18:08 -0300

    11

--- Jenkinsfile
+++ Jenkinsfile
@@ -48,13 +48,16 @@ pipeline {
                 }
             }
         }
-        
-        // stage('Deploying App to Kubernetes') {
-        //     steps {
-        //         script {
-        //             kubernetesDeploy(configs: "deploymentservice.yml", kubeconfigId: "kubernetes"                    
-        //         }
-        //     }
-        // }
+
+        stage('Deploying App to Kubernetes') {
+            steps {
+                script {
+                    sh ""
+                    kubectl apply -f deployments
+                    kubectl apply -f services
+                    kubectl appl -f ingress                   
+                }
+            }
+        }
     }
 }
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-11-jre -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
+apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.13/2022-10-31/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
